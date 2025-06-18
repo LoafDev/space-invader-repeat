a0720db02dc4c955491eeca904cd09e91c6d3c4b
Vietphogn <vietphogn@gmail.com>
2025-06-18 22:30:18 +0700

    random ahh changes in apple.h

--- src/apple.c
+++ src/apple.c
@@ -3,14 +3,13 @@
 
 void get_random_position(struct Apple *apple)
 {
-    apple->x = GetRandomValue(0, 875);
-    apple->y = GetRandomValue(0, 875);
+    apple->x = GetRandomValue(0, 864);
+    apple->y = GetRandomValue(0, 864);
 }
 
 void draw(struct Apple *apple)
 {
-    get_random_position(apple);
-    Rectangle rect = {(float)apple->x, (float)apple->y, 25.0f, 25.0f};
+    Rectangle rect = {(float)apple->x, (float)apple->y, 36.0f, 36.0f};
     Color red = {250, 56, 42, 255};
     DrawRectangleRounded(rect, 0.3, 20, red);
 }
